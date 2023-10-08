6650de5dc966c98860e1e13e70471b761a44c7ab
zoulinchuan <zoulinchaun2004@163.com>
2023-10-08 20:10:01 +0800

    2023/10/8

---
+++ src/test/java/co/zou/FirstSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package co.zou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
